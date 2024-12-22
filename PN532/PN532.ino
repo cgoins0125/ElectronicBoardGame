cde1c0cc0c6cb8f7f36e70ffc75d4aeb51c11d4a
danae <nmn952@mocs.utc.edu>
2024-12-22 12:02:05 -0500

    Changed to simpler example.

--- PN532/PN532.ino
+++ PN532/PN532.ino
@@ -1,28 +1,22 @@
 /**************************************************************************/
 /*!
-    @file     iso14443as_target.pde
-    @original Adafruit Industries
-    @modified Salvador Mendoza(@Netxing)
+    @file     readntag203.pde
+    @author   KTOWN (Adafruit Industries)
     @license  BSD (see license.txt)
 
-    This example will attempt to mimic an ISO14443A smart card
-    and retrieve some basic information from a PoS or terminal,
-    this can be used to establish a communication process.
+    This example will wait for any NTAG203 or NTAG213 card or tag,
+    and will attempt to read from it.
 
-    Note that you need the baud rate to be 115200 because we need to print
-    out the data and read from the card at the same time!
+    This is an example sketch for the Adafruit PN532 NFC/RFID breakout boards
+    This library works with the Adafruit NFC breakout
+      ----> https://www.adafruit.com/products/364
 
-This is an example sketch for the Adafruit PN532 NFC/RFID breakout boards
-This library works with the Adafruit NFC breakout
-  ----> https://www.adafruit.com/products/364
+    Check out the links above for our tutorials and wiring diagrams
+    These chips use SPI or I2C to communicate.
 
-Check out the links above for our tutorials and wiring diagrams
-These chips use SPI or I2C to communicate.
-
-Adafruit invests time and resources providing this open source code,
-please support Adafruit and open-source hardware by purchasing
-products from Adafruit!
-
+    Adafruit invests time and resources providing this open source code,
+    please support Adafruit and open-source hardware by purchasing
+    products from Adafruit!
 */
 /**************************************************************************/
 #include <Wire.h>
@@ -30,7 +24,10 @@ products from Adafruit!
 #include <Adafruit_PN532.h>
 
 // If using the breakout with SPI, define the pins for SPI communication.
+//#define PN532_SCK  (2)
+//#define PN532_MOSI (3)
 #define PN532_SS   (10)
+//#define PN532_MISO (5)
 
 // If using the breakout or shield with I2C, define just the pins connected
 // to the IRQ and reset lines.  Use the values below (2, 3) for the shield!
@@ -40,7 +37,7 @@ products from Adafruit!
 // Uncomment just _one_ line below depending on how your breakout or shield
 // is connected to the Arduino:
 
-// Use this line for a breakout with a SPI connection:
+// Use this line for a breakout with a software SPI connection (recommended):
 //Adafruit_PN532 nfc(PN532_SCK, PN532_MISO, PN532_MOSI, PN532_SS);
 
 // Use this line for a breakout with a hardware SPI connection.  Note that
@@ -51,6 +48,7 @@ Adafruit_PN532 nfc(PN532_SS);
 
 // Or use this line for a breakout or shield with an I2C connection:
 //Adafruit_PN532 nfc(PN532_IRQ, PN532_RESET);
+
 
 void setup(void) {
   Serial.begin(115200);
@@ -65,39 +63,92 @@ void setup(void) {
     Serial.print("Didn't find PN53x board");
     while (1); // halt
   }
-
   // Got ok data, print it out!
   Serial.print("Found chip PN5"); Serial.println((versiondata>>24) & 0xFF, HEX);
   Serial.print("Firmware ver. "); Serial.print((versiondata>>16) & 0xFF, DEC);
   Serial.print('.'); Serial.println((versiondata>>8) & 0xFF, DEC);
 
-  // Set the max number of retry attempts to read from a card
-  // This prevents us from waiting forever for a card, which is
-  // the default behaviour of the PN532.
-  nfc.setPassiveActivationRetries(0xFF);
-
-  Serial.println("As Target... Approach the NFC PN532 Board to a PoS or terminal!");
-  delay(200);
+  Serial.println("Waiting for an ISO14443A Card ...");
 }
 
 void loop(void) {
-  uint8_t apdubuffer[255] = {}, apdulen = 0;
-  uint8_t ppse[] = {0x8E, 0x6F, 0x23, 0x84, 0x0E, 0x32, 0x50, 0x41, 0x59, 0x2E, 0x53, 0x59, 0x53, 0x2E, 0x44, 0x44, 0x46, 0x30, 0x31, 0xA5, 0x11, 0xBF, 0x0C, 0x0E, 0x61, 0x0C, 0x4F, 0x07, 0xA0, 0x00, 0x00, 0x00, 0x03, 0x10, 0x10, 0x87, 0x01, 0x01, 0x90, 0x00};
-  nfc.AsTarget();
-  (void)nfc.getDataTarget(apdubuffer, &apdulen); //Read initial APDU
-  if (apdulen>0){
-    for (uint8_t i = 0; i < apdulen; i++){
-      Serial.print(" 0x"); Serial.print(apdubuffer[i], HEX);
-    }
+  uint8_t success;
+  uint8_t uid[] = { 0, 0, 0, 0, 0, 0, 0 };  // Buffer to store the returned UID
+  uint8_t uidLength;                        // Length of the UID (4 or 7 bytes depending on ISO14443A card type)
+
+  // Wait for an NTAG203 card.  When one is found 'uid' will be populated with
+  // the UID, and uidLength will indicate the size of the UUID (normally 7)
+  success = nfc.readPassiveTargetID(PN532_MIFARE_ISO14443A, uid, &uidLength);
+
+  if (success) {
+    // Display some basic information about the card
+    Serial.println("Found an ISO14443A card");
+    Serial.print("  UID Length: ");Serial.print(uidLength, DEC);Serial.println(" bytes");
+    Serial.print("  UID Value: ");
+    nfc.PrintHex(uid, uidLength);
     Serial.println("");
-  }
-  nfc.setDataTarget(ppse, sizeof(ppse));   //Mimic a smart card response with a PPSE APDU
-  nfc.getDataTarget(apdubuffer, &apdulen); //Read respond from the PoS or Terminal
-  if (apdulen>0){
-    for (uint8_t i = 0; i < apdulen; i++){
-      Serial.print(" 0x"); Serial.print(apdubuffer[i], HEX);
+
+    if (uidLength == 7)
+    {
+      uint8_t data[32];
+
+      // We probably have an NTAG2xx card (though it could be Ultralight as well)
+      Serial.println("Seems to be an NTAG2xx tag (7 byte UID)");
+
+      // NTAG2x3 cards have 39*4 bytes of user pages (156 user bytes),
+      // starting at page 4 ... larger cards just add pages to the end of
+      // this range:
+
+      // See: http://www.nxp.com/documents/short_data_sheet/NTAG203_SDS.pdf
+
+      // TAG Type       PAGES   USER START    USER STOP
+      // --------       -----   ----------    ---------
+      // NTAG 203       42      4             39
+      // NTAG 213       45      4             39
+      // NTAG 215       135     4             129
+      // NTAG 216       231     4             225
+
+      for (uint8_t i = 0; i < 42; i++)
+      {
+        success = nfc.ntag2xx_ReadPage(i, data);
+
+        // Display the current page number
+        Serial.print("PAGE ");
+        if (i < 10)
+        {
+          Serial.print("0");
+          Serial.print(i);
+        }
+        else
+        {
+          Serial.print(i);
+        }
+        Serial.print(": ");
+
+        // Display the results, depending on 'success'
+        if (success)
+        {
+          // Dump the page data
+          nfc.PrintHexChar(data, 4);
+        }
+        else
+        {
+          Serial.println("Unable to read the requested page!");
+        }
+      }
     }
-    Serial.println("");
+    else
+    {
+      Serial.println("This doesn't seem to be an NTAG203 tag (UUID length != 7 bytes)!");
+    }
+
+    // Wait a bit before trying again
+    Serial.println("\n\nSend a character to scan another tag!");
+    Serial.flush();
+    while (!Serial.available());
+    while (Serial.available()) {
+    Serial.read();
+    }
+    Serial.flush();
   }
-  delay(1000);
 }
