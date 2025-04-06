aef71ab9e8cd0b8f577b9dcf287606aec8406d8b
Casey Goins <cgoins0125@hotmail.com>
2025-04-05 20:52:46 -0400

    updates to lighting methods

--- libraries/HardwareAPI/HardwareAPI.cpp
+++ libraries/HardwareAPI/HardwareAPI.cpp
@@ -1877,17 +1877,70 @@ void HardwareAPI::runSpiralPattern(char color)
 
 void HardwareAPI::runRowSweepPattern(char color) 
 {
+  std::vector<char> r1tiles = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07};
+  std::vector<char> r2tiles = {0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17};
+  std::vector<char> r3tiles = {0x20, 0x21, 0x22, 0x23, 0x24, 0x25, 0x26, 0x27};
+  std::vector<char> r4tiles = {0x30, 0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37};
+  std::vector<char> r5tiles = {0x40, 0x41, 0x42, 0x43, 0x44, 0x45, 0x46, 0x47};
+  std::vector<char> r6tiles = {0x50, 0x51, 0x52, 0x53, 0x54, 0x55, 0x56, 0x57};
+  std::vector<char> r7tiles = {0x60, 0x61, 0x62, 0x63, 0x64, 0x65, 0x66, 0x67};
+  std::vector<char> r8tiles = {0x70, 0x71, 0x72, 0x73, 0x74, 0x75, 0x76, 0x77};
+  std::vector<std::vector<char>> rows = {r1tiles, r2tiles, r3tiles, r4tiles, r5tiles, r6tiles, r7tiles, r8tiles};
 
+  // Define colors
+  std::array<char, 3> colors = {'R', 'B', 'Y'};  //Red, Blue, Yellow
+
+  // Loop through all rows
+  for (auto& currentRow : rows) {
+    //Turn each tile green
+    turnOnMultipleTiles(currentRow, 'G');
+    //Wait a sec
+    delay(500);
+    
+    // Iterate over each color
+    for (char color : colors) {
+      
+      // Turn on the LEDs in the current row with the current color
+      changeMultipleTileColors(currentRow, color)
+      
+      // Wait for 1 seconds
+      delay(500);
+    }
+    // Turn off all LEDs for the current row before moving to the next row
+    turnOffMultipleTiles(currentRow);
 }
 
 void HardwareAPI::runDiagonalPattern(char color) 
 {
-
-}
-
-void HardwareAPI::runRandomBlinkPattern(char color) 
-{
-
+  std::vector<char> d1tiles = {0x00};
+  std::vector<char> d2tiles = {0x10, 0x01};
+  std::vector<char> d3tiles = {0x20, 0x11, 0x02};
+  std::vector<char> d4tiles = {0x30, 0x21, 0x12, 0x03};
+  std::vector<char> d5tiles = {0x40, 0x31, 0x22, 0x13, 0x04};
+  std::vector<char> d6tiles = {0x50, 0x41, 0x32, 0x23, 0x14, 0x05};
+  std::vector<char> d7tiles = {0x60, 0x51, 0x42, 0x33, 0x24, 0x15, 0x06};
+  std::vector<char> d8tiles = {0x70, 0x61, 0x52, 0x43, 0x34, 0x25, 0x16, 0x07};
+  std::vector<char> d9tiles = {0x71, 0x62, 0x53, 0x44, 0x35, 0x26, 0x17};
+  std::vector<char> d10tiles = {0x72, 0x63, 0x54, 0x45, 0x36, 0x27};
+  std::vector<char> d11tiles = {0x73, 0x64, 0x55, 0x46, 0x37};
+  std::vector<char> d12tiles = {0x74, 0x65, 0x56, 0x47};
+  std::vector<char> d13tiles = {0x75, 0x66, 0x57};
+  std::vector<char> d14tiles = {0x76, 0x67};
+  std::vector<char> d15tiles = {0x76};
+  std::vector<std::vector<char>> lowerDiagonals = {d1tiles, d2tiles, d3tiles, d4tiles, d5tiles, d6tiles, d7tiles, d8tiles, d9tiles, d10tiles, d11tiles, d12tiles, d13tiles, d14tiles};
+  
+  for (int time = 1000 ; time > 0 ; time -= 100) 
+  {
+    for (auto& currentDiagonal : diagonals) {
+      //Turn each tile green
+      turnOnMultipleTiles(currentDiagonal, color);
+      //Wait a sec
+      delay(time);
+      
+      turnOffMultipleTiles(currentRow); 
+    }
+    // Turn off all LEDs for the current row before moving to the next row 
+  }
 }
 
 char HardwareAPI::hexTile(int row, int col) 
